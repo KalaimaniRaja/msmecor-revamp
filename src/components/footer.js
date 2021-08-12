dafc61edf5227bc7b3ef42061a50ac926ec7edc0
kalaimani <kalaimani.b@techmango.net>
2021-08-12 12:02:25 +0530

    Home page design

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,28 @@
 import React from "react";
+import { Row, Col } from "antd";
+import PlayStore from "../assets/img/play-store.png";
+import AppStore from "../assets/img/app-store.png";
+import Logo from "../assets/img/logo.png";
 
 class Footer extends React.Component {
   render() {
     return (
-      <div className="footer-content">
-        footer content
+      <div className="footer-content py-4 py-md-5">
+        <div className="container">
+          <Row type="flex" align="middle" justify="center">
+            <Col>
+              <img className="logo" src={Logo} alt="" />
+            </Col>
+          </Row>
+          <Row>
+            <Col md={{ span: 12 }} className="text-md-end">
+              <img src={PlayStore} alt="" />
+            </Col>
+            <Col md={{ span: 12 }} className="text-md-start">
+              <img src={AppStore} alt="" />
+            </Col>
+          </Row>
+        </div>
       </div>
     );
   }
